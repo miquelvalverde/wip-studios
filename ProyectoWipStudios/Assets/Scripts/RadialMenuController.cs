4bb27e30688446b5d58f092d859572b187f57fde
miquelvalverde <pianoman9000@gmail.com>
2020-04-21 21:18:46 +0200

    created dynamic radial menu with customizable icons

---
+++ ProyectoWipStudios/Assets/Scripts/RadialMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialMenuController : MonoBehaviour
+{
+    [SerializeField] private RadialMenuPortionScriptable[] portions;
+    [SerializeField] private RadialMenuPortion portionPrefabRef;
+    [SerializeField] private float iconDistanceFromCenter;
+        
+    private void Start()
+    {
+        CreateWheel();
+    }
+
+    private void CreateWheel()
+    {
+        var portionCount = portions.Length;
+        var portionSize360 = 360 / portionCount;
+        var portionSize01 = portionSize360 / 360.0f;
+        for (int portionIndex = 0; portionIndex < portionCount; portionIndex++)
+        {
+            CreatePortion(portionIndex, portionSize01, portionSize360, portions[portionIndex]);
+        }
+    }
+
+    private void CreatePortion(int index, float size01, float size360, RadialMenuPortionScriptable settings)
+    {   
+        var portion = Instantiate(portionPrefabRef, this.transform) as RadialMenuPortion;
+        portion.icon.sprite = settings.icon;
+        portion.iconRect.localPosition = new Vector3(0, iconDistanceFromCenter, 0);
+        portion.iconPivot.rotation = Quaternion.AngleAxis(-size360 / 2, Vector3.forward);
+        portion.background.color = settings.background;
+        portion.background.fillAmount = size01;
+        portion.backgroundRect.rotation = Quaternion.AngleAxis(index * size360, Vector3.forward);
+    }
+}
---
+++ ProyectoWipStudios/Assets/Scripts/RadialMenuPortion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+    
+public class RadialMenuPortion : MonoBehaviour
+{
+    public Image icon;
+    public RectTransform iconRect;
+    public RectTransform iconPivot;
+    public Image background;
+    public RectTransform backgroundRect;
+}
