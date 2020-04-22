a52e21155d46e3468a86ea66806a62477911c93b
Dídac <didacpadilla20@gmail.com>
2020-04-22 17:03:52 +0200

    Start working on Interactable trees

---
+++ ProyectoWipStudios/Assets/Scripts/Interactables/TreeInteractable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeInteractable : AInteractable
+{
+
+    [SerializeField] private List<Transform> climbPoints = new List<Transform>();
+
+}
