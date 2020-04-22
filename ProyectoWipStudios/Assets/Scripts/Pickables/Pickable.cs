ac2be40e8b731fff981d7b8cf3eb221c42dc1a91
miquelvalverde <pianoman9000@gmail.com>
2020-04-22 23:08:20 +0200

    added tongue model and retractile movement

---
+++ ProyectoWipStudios/Assets/Scripts/Pickables/Pickable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    public void Pick()
+    {
+    }
+}
---
+++ ProyectoWipStudios/Assets/Scripts/Player/Animals/Chameleon/Tongue.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tongue : MonoBehaviour
+{
+    [SerializeField] private Transform tongueScaler;
+    [SerializeField] private AnimationCurve movementCurve;
+    [SerializeField] private float speed;
+    [SerializeField] private Transform tongueEnd;
+    private bool isProtracting;
+    private bool isRetracting;
+    private float pickableDistance;
+    private float currentTime;
+    private Pickable closestPickable;
+        
+    private void Awake()
+    {
+       ResetTongue();
+    }   
+
+    private void ResetTongue()
+    {
+        tongueScaler.localPosition = Vector3.zero;
+        tongueScaler.localScale = Vector3.one;
+        tongueScaler.localRotation = Quaternion.identity;
+        isProtracting = false;
+        isRetracting = false;
+        currentTime = 0;
+        tongueScaler.gameObject.SetActive(false);
+        closestPickable = null;
+    }
+
+    public void ProtractTongue(Pickable toPickable)
+    {
+        if(toPickable == null)
+            return;
+
+        var direction = toPickable.transform.position - tongueScaler.position;
+        if (Vector3.Dot(direction, transform.forward) < 0)
+            return;
+
+        tongueScaler.forward = direction;
+        pickableDistance = Vector3.Distance(tongueScaler.position, toPickable.transform.position);
+        isProtracting = true;
+        tongueScaler.gameObject.SetActive(true);
+    }
+
+    private void RetractTongue()
+    {
+        isRetracting = true;
+        closestPickable.transform.SetParent(tongueEnd);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            closestPickable = GetClosestPickable();
+            ProtractTongue(closestPickable);
+        }
+
+        currentTime += Time.deltaTime;
+        float factor = movementCurve.Evaluate(currentTime);
+
+        if (isProtracting)
+        {
+            tongueScaler.localScale += new Vector3(0, 0, speed * factor * Time.deltaTime);
+
+            if(tongueScaler.localScale.z >= pickableDistance)
+            {
+                isProtracting = false;
+                currentTime = 0;
+                RetractTongue();
+            }
+        }
+        else if (isRetracting)
+        {
+            tongueScaler.localScale -= new Vector3(0, 0, speed * factor * Time.deltaTime);
+            if(closestPickable)
+                closestPickable.transform.localScale = Vector3.one;
+
+            if (tongueScaler.localScale.z <= 1)
+            {
+                isRetracting = false;
+                ResetTongue();
+                if(closestPickable)
+                {
+                    Destroy(closestPickable.gameObject);
+                    closestPickable = null;
+                }
+            }
+        }
+    }
+
+    private Pickable GetClosestPickable()
+    {
+        float minDistance = float.MaxValue;
+        Pickable closestPickable = null;
+        foreach (Pickable pickable in GameObject.FindObjectsOfType<Pickable>())
+        {
+            float distance = Vector3.Distance(tongueScaler.position, pickable.transform.position);
+            if (distance < minDistance)
+            {
+                closestPickable = pickable;
+                minDistance = distance;
+            }
+        }
+        return closestPickable;
+    }
+}
