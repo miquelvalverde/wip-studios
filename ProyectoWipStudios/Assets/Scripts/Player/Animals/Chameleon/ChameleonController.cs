68de997da6e738eb2d8020e65a205e3c0198eca6
miquelvalverde <pianoman9000@gmail.com>
2020-04-24 16:09:32 +0200

    created chameleon controller

---
+++ ProyectoWipStudios/Assets/Scripts/Player/Animals/Chameleon/ChameleonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChameleonController : PlayerSpecificController
+{
+    public override void Initializate(InputSystem controls)
+    {
+    }
+
+    public override void UpdateSpecificAction()
+    {
+    }
+}
