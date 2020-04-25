ebeb70946ca2d9831308412e40b3f71328cc5bc8
miquelvalverde <pianoman9000@gmail.com>
2020-04-25 13:41:04 +0200

    created destructible rock in maya and imported

---
+++ ProyectoWipStudios/Assets/Scripts/Destructible.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destructibleVersion;
+
+    private void Update()
+    {
+        // TODO delete. Only used to test. 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Break();
+        }
+    }
+
+    public void Break()
+    {
+        Instantiate(destructibleVersion, transform.position, transform.rotation);
+        this.gameObject.SetActive(false);  
+    }
+}
