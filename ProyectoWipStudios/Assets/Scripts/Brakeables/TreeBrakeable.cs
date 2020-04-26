5196f7e32a8bffbab1785ab22154decec6dcecac
miquelvalverde <pianoman9000@gmail.com>
2020-04-26 18:09:01 +0200

    tree falling according to angle, time and curve

--- ProyectoWipStudios/Assets/Scripts/Brakeables/TreeBrakeable.cs
+++ ProyectoWipStudios/Assets/Scripts/Brakeables/TreeBrakeable.cs
@@ -6,20 +6,47 @@ using UnityEngine;
 public class TreeBrakeable : MonoBehaviour, IBreakable
 {
     [SerializeField] private float fallAngle;
+    [SerializeField] private float fallTime;    
+    [SerializeField] private AnimationCurve fallCurve;
     private TreeInteractable treeInteractable;
     private float debugHeight = 10F;
     private int debugArrowCount = 5;
     private float debugArrowLength = 2F;
-    private Quaternion fallRotation;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Break();
+        }
+    }
 
     public void Break()
     {
-        treeInteractable.CanInteract = false;
+        if(treeInteractable.CanInteract)
+        {
+            treeInteractable.CanInteract = false;
+            StartCoroutine(TreeFallRoutine());
+        }
     }
 
     private void Awake()
     {
         treeInteractable = GetComponent<TreeInteractable>();
+    }
+
+    IEnumerator TreeFallRoutine()
+    {
+        var direction = transform.right;
+        direction = Quaternion.AngleAxis(fallAngle, transform.up) * direction;
+        var currentDirection = transform.up;
+        float timer = 0.0f;
+        while (timer <= fallTime)
+        {
+            transform.up = Vector3.Lerp(currentDirection, direction, fallCurve.Evaluate(timer / fallTime));
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 
     private void OnDrawGizmos()
@@ -33,5 +60,4 @@ public class TreeBrakeable : MonoBehaviour, IBreakable
             DrawArrow.ForGizmo(currentPosition += deltaY, direction * debugArrowLength);
         }
     }
-
 }
