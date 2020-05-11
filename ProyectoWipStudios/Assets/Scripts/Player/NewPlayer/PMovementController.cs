f16b0f60a69bdc4fcd8e9e213e942958e82237df
Dídac <didacpadilla20@gmail.com>
2020-05-11 19:44:19 +0200

    More shit

--- ProyectoWipStudios/Assets/Scripts/Player/NewPlayer/PMovementController.cs
+++ ProyectoWipStudios/Assets/Scripts/Player/NewPlayer/PMovementController.cs
@@ -9,11 +9,21 @@ public class PMovementController : AMonoBehaivourWithInputs
 
     [Header("Walk")]
     private Vector2 moveInput;
+    [SerializeField] private float acceleration = .1f;
+    [SerializeField] private float deceleration = .05f;
+    private float speedSmoothVelocity;
+    private float currentSpeed;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
 
+    }
+
+    protected override void SetControls()
+    {
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += _ => moveInput = Vector2.zero;
     }
 
     public void DoUpdate()
@@ -24,6 +34,13 @@ public class PMovementController : AMonoBehaivourWithInputs
     public void DoFixedUpdate()
     {
         
+    }
+
+    private void CalculateNormalMovement()
+    {
+        Vector3 targetSpeed = player.stats.speed * moveInput;
+        targetSpeed.y = rb.velocity.y;
+        //currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed.mag, ref speedSmoothVelocity, acceleration);
     }
 
 }
