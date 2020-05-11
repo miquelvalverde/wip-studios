991de34a8914e6226c2beb34b0682873c4a10d7a
miquelvalverde <pianoman9000@gmail.com>
2020-05-11 19:48:59 +0200

    story

---
+++ ProyectoWipStudios/Assets/Scripts/HUD/StoryScriptable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "KODAKI/Story", order = 1)]
+public class StoryScriptable : ScriptableObject
+{
+    [TextArea]
+    public List<string> sentences;
+}
