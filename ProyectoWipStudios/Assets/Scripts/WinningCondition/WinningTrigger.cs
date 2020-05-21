626ab2a057a1879f38850aad771c84e3b9715e45
Dídac <didacpadilla20@gmail.com>
2020-05-21 21:56:29 +0200

    Winning Condition Added

---
+++ ProyectoWipStudios/Assets/Scripts/WinningCondition/WinningTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinningTrigger : MonoBehaviour
+{
+    [SerializeField] private int sceneIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+
+}
