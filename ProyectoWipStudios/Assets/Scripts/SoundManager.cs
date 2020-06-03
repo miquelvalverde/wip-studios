e8eafc5c8ab52ab66db34ab7cda6b4bb16c3dc0e
miquelvalverde <pianoman9000@gmail.com>
2020-06-03 16:12:41 +0200

    added pause sounds and buttons, and fixed some more FMOD errors...

---
+++ ProyectoWipStudios/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    #region Singleton
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(this);
+    }
+    #endregion
+
+    public static FMOD.Studio.EventInstance UIHover;
+    public static FMOD.Studio.EventInstance UISelect;
+
+    private void Start()
+    {
+        LoadAllFMODSounds();
+    }
+
+    private void LoadAllFMODSounds()
+    {
+        UISelect = FMODUnity.RuntimeManager.CreateInstance("event:/Events/UI/Select item/ui_select");
+        UIHover = FMODUnity.RuntimeManager.CreateInstance("event:/Events/UI/Hover item/ui_hover");
+    }
+}
---
+++ ProyectoWipStudios/Assets/Scripts/TestFmod.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFmod : MonoBehaviour
+{
+    FMOD.Studio.EventInstance PlayMySound;
+
+    private void Awake()
+    {
+        PlayMySound = FMODUnity.RuntimeManager.CreateInstance("event:/Events/UI/Select item/ui_select");
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("triggered sound");
+            PlayMySound.start();
+        }
+    }
+}
