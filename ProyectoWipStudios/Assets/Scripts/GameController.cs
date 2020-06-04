7c699f51a838805db84ef4792000dd7f7538c757
miquelvalverde <pianoman9000@gmail.com>
2020-06-04 20:08:22 +0200

    win when press button

--- ProyectoWipStudios/Assets/Scripts/GameController.cs
+++ ProyectoWipStudios/Assets/Scripts/GameController.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaivourWithInputs
 {
     [SerializeField] private GameObject pauseMenuHUD = null;
+
+    public static GameController instance;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+    }
 
     void Start()
     {
@@ -31,6 +46,13 @@ public class GameController : MonoBehaivourWithInputs
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         SoundManager.Music2.start();
+    }
+
+    public void GameWin()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        SceneManager.LoadScene(1);
     }
         
     public void GameQuit()
